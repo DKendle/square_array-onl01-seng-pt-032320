fdb7dd3508b590259685a52d5f102d78c4d0875a
Dina Kendle <dina.kendle@gmail.com>
2020-03-30 22:19:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-def square_array(numbers)
+def square_array(new_numbers)
   new_numbers = Array.new()
   numbers.each do |number| 
     new_numbers << "#{number} ** 2"
   end
-  new_numbers
+  
 end
 
 
